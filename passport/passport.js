aa695a8717dc1968b5b8d0731d61b637f81cdb92
Mary Casasola <marycruzc18@gmail.com>
2023-09-17 00:47:04 -0300

    Sexto Desafío

--- passport/passport.js
+++ passport/passport.js
@@ -1,5 +1,6 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
+import GitHubStrategy from 'passport-github2';
 import User from '../dao/models/user.model.js';
 
 const initializePassport = () => {
@@ -31,5 +32,38 @@ const initializePassport = () => {
     });
   });
 };
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ githubId: profile.id });
+
+        if (!user) {
+         
+          user = new User({
+            githubId: profile.id,
+            username: profile.username,
+           
+          });
+          await user.save();
+        }
+
+        
+        return done(null, user);
+      } catch (error) {
+      
+        return done(error);
+      }
+    }
+  )
+);
+
+
 
 export default initializePassport;
